2fb711255b2baa144e3edb22429d4bc03127c5dc
danwristband <59277358+danwristband@users.noreply.github.com>
2020-01-10 17:22:32 +0700

    test

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+
+int main()
+{   double x;
+    double sum; 
+    while(x!=0){   
+
+
+    cout<<"Enter x:";
+    cin>>x;
+    if(x>0){
+    sum=sum+x;
+         }
+
+        }  
+cout<<"sum = "<<sum;
+
+if(x==0)
+    return 0;
+}
